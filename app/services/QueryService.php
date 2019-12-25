6d76e667e0dbc2250c6c302d7b3e90939d0e9659
luoxiaojun <luoxiaojun@dodoca.com>
2019-12-25 18:13:30 +0800

    update demo

--- app/services/QueryService.php
+++ app/services/QueryService.php
@@ -16,98 +16,98 @@ class QueryService extends BaseService
 {
     public function select()
     {
-        $redis = RedisPool::pick('pika');
-        try {
-            for ($i = 0; $i < 1000000; ++$i) {
-                $firstAlphabet = chr(ord('a') + ($i % 25));
-                $redis->hSet('test2.name', $firstAlphabet . 'oo', json_encode([['id' => $i]]));
-                $redis->hSet('test2', $i, json_encode(['id' => $i, 'type' => 1, 'name' => $firstAlphabet . 'oo']));
-                $redis->hSet('test1.name', $firstAlphabet . 'oo', json_encode([['id' => $i]]));
-                $redis->hSet('test1', $i, json_encode(['id' => $i, 'type' => 1, 'name' => $firstAlphabet . 'oo']));
-            }
-
-            $redis->hSet('meta.schema', 'test1', json_encode([
-                'pk' => 'id',
-                'columns' => [
-                    [
-                        'name' => 'id',
-                        'type' => 'int',
-                        'length' => 11,
-                        'default' => null,
-                        'allow_null' => false,
-                    ],
-                    [
-                        'name' => 'type',
-                        'type' => 'int',
-                        'length' => 11,
-                        'default' => 0,
-                        'allow_null' => false,
-                    ],
-                    [
-                        'name' => 'name',
-                        'type' => 'varchar',
-                        'length' => 255,
-                        'default' => '',
-                        'allow_null' => false,
-                    ],
-                ],
-                'index' => [
-                    [
-                        'name' => 'name',
-                        'columns' => ['name'],
-                        'unique' => false,
-                    ],
-                ],
-            ]));
-            $redis->hSet('meta.schema', 'test2', json_encode([
-                'pk' => 'id',
-                'columns' => [
-                    [
-                        'name' => 'id',
-                        'type' => 'int',
-                        'length' => 11,
-                        'default' => null,
-                        'allow_null' => false,
-                    ],
-                    [
-                        'name' => 'type',
-                        'type' => 'int',
-                        'length' => 11,
-                        'default' => 0,
-                        'allow_null' => false,
-                    ],
-                    [
-                        'name' => 'name',
-                        'type' => 'varchar',
-                        'length' => 255,
-                        'default' => '',
-                        'allow_null' => false,
-                    ],
-                ],
-                'index' => [
-                    [
-                        'name' => 'name',
-                        'columns' => ['name'],
-                        'unique' => false,
-                    ],
-                ],
-            ]));
-            var_dump($redis->hGet('meta.schema', 'test1'));
-            var_dump($redis->hGet('meta.schema', 'test2'));
-
-            return [
-                'code' => 0,
-                'msg' => 'ok',
-                'data' => [
-                    'result_set' => [],
-                ],
-            ];
-        } catch (\Throwable $e) {
-            throw $e;
-        } finally {
-            RedisPool::release($redis);
-        }
-        return;
+//        $redis = RedisPool::pick('pika');
+//        try {
+//            for ($i = 0; $i < 1000000; ++$i) {
+//                $firstAlphabet = chr(ord('a') + ($i % 25));
+//                $redis->hSet('test2.name', $firstAlphabet . 'oo', json_encode([['id' => $i]]));
+//                $redis->hSet('test2', $i, json_encode(['id' => $i, 'type' => 1, 'name' => $firstAlphabet . 'oo']));
+//                $redis->hSet('test1.name', $firstAlphabet . 'oo', json_encode([['id' => $i]]));
+//                $redis->hSet('test1', $i, json_encode(['id' => $i, 'type' => 1, 'name' => $firstAlphabet . 'oo']));
+//            }
+//
+//            $redis->hSet('meta.schema', 'test1', json_encode([
+//                'pk' => 'id',
+//                'columns' => [
+//                    [
+//                        'name' => 'id',
+//                        'type' => 'int',
+//                        'length' => 11,
+//                        'default' => null,
+//                        'allow_null' => false,
+//                    ],
+//                    [
+//                        'name' => 'type',
+//                        'type' => 'int',
+//                        'length' => 11,
+//                        'default' => 0,
+//                        'allow_null' => false,
+//                    ],
+//                    [
+//                        'name' => 'name',
+//                        'type' => 'varchar',
+//                        'length' => 255,
+//                        'default' => '',
+//                        'allow_null' => false,
+//                    ],
+//                ],
+//                'index' => [
+//                    [
+//                        'name' => 'name',
+//                        'columns' => ['name'],
+//                        'unique' => false,
+//                    ],
+//                ],
+//            ]));
+//            $redis->hSet('meta.schema', 'test2', json_encode([
+//                'pk' => 'id',
+//                'columns' => [
+//                    [
+//                        'name' => 'id',
+//                        'type' => 'int',
+//                        'length' => 11,
+//                        'default' => null,
+//                        'allow_null' => false,
+//                    ],
+//                    [
+//                        'name' => 'type',
+//                        'type' => 'int',
+//                        'length' => 11,
+//                        'default' => 0,
+//                        'allow_null' => false,
+//                    ],
+//                    [
+//                        'name' => 'name',
+//                        'type' => 'varchar',
+//                        'length' => 255,
+//                        'default' => '',
+//                        'allow_null' => false,
+//                    ],
+//                ],
+//                'index' => [
+//                    [
+//                        'name' => 'name',
+//                        'columns' => ['name'],
+//                        'unique' => false,
+//                    ],
+//                ],
+//            ]));
+//            var_dump($redis->hGet('meta.schema', 'test1'));
+//            var_dump($redis->hGet('meta.schema', 'test2'));
+//
+//            return [
+//                'code' => 0,
+//                'msg' => 'ok',
+//                'data' => [
+//                    'result_set' => [],
+//                ],
+//            ];
+//        } catch (\Throwable $e) {
+//            throw $e;
+//        } finally {
+//            RedisPool::release($redis);
+//        }
+//        return;
 
 
 //        $index = RedisPool::pick('pika');
