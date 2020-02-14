1609c9ca02cef798b4676f3a01c4a560b2e808f7
luoxiaojun <luoxiaojun@dodoca.com>
2020-02-14 15:12:08 +0800

    insert

--- app/components/plans/InsertPlan.php
+++ app/components/plans/InsertPlan.php
@@ -2,7 +2,36 @@
 
 namespace App\components\plans;
 
+use App\components\Ast;
+use App\components\elements\Column;
+use App\components\storage\AbstractStorage;
+
 class InsertPlan
 {
-    //todo
+    /** @var Ast */
+    protected $ast;
+
+    /** @var AbstractStorage */
+    protected $storage;
+
+    /** @var Column[] */
+    protected $columns = [];
+
+    protected $schemas;
+
+    public function __construct(Ast $ast, AbstractStorage $storage)
+    {
+        $this->ast = $ast;
+
+        //todo sql校验
+    }
+
+    /**
+     * @return array|mixed
+     * @throws \Throwable
+     */
+    public function execute()
+    {
+        //todo
+    }
 }
