bab47b2472ef4ce45c19ace7e97ce7a7186227ee
luoxiaojun <luoxiaojun@dodoca.com>
2020-03-07 23:54:18 +0800

    update function

--- app/components/plans/UpdatePlan.php
+++ app/components/plans/UpdatePlan.php
@@ -2,7 +2,32 @@
 
 namespace App\components\plans;
 
+use App\components\Ast;
+use App\components\elements\condition\Condition;
+use App\components\elements\condition\ConditionTree;
+use App\components\storage\AbstractStorage;
+
 class UpdatePlan
 {
     //todo
+
+    /** @var Ast */
+    protected $ast;
+
+    /** @var AbstractStorage */
+    protected $storage;
+
+    /** @var Condition|ConditionTree|null  */
+    protected $condition;
+
+    /**
+     * UpdatePlan constructor.
+     * @param Ast $ast
+     * @param AbstractStorage $storage
+     */
+    public function __construct(Ast $ast, AbstractStorage $storage)
+    {
+        $this->ast = $ast;
+        $this->storage = $storage;
+    }
 }
