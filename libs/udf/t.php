cdeb39ee86cd05063cf4a6aff7f1605bf38f078b
luoxiaojun <luoxiaojun@dodoca.com>
2020-03-15 19:29:17 +0800

    ffi udf

--- libs/udf/t.php
+++ libs/udf/t.php
@@ -4,6 +4,8 @@ $cUdf = FFI::cdef(
 <<<EOF
 double ArraySum(double numbers[], int size);
 double ArrayAvg(double numbers[], int size);
+double ArrayMin(double numbers[], int size);
+double ArrayMax(double numbers[], int size);
 EOF, __DIR__ . '/libcudf.so'
 );
 
@@ -54,4 +56,26 @@ echo 'Usage time:', (microtime(true) - $start) * 1000, 'ms', PHP_EOL;
 $start = microtime(true);
 echo 'Calculate avg of 10 billion doubles using PHP', PHP_EOL;
 var_dump(array_sum($arr) / 100000000);
+echo 'Usage time:', (microtime(true) - $start) * 1000, 'ms', PHP_EOL;
+
+//Test Min
+$start = microtime(true);
+echo 'Calculate min of 10 billion doubles using C', PHP_EOL;
+var_dump($cUdf->ArrayMin($cArr, 100000000));
+echo 'Usage time:', (microtime(true) - $start) * 1000, 'ms', PHP_EOL;
+
+$start = microtime(true);
+echo 'Calculate min of 10 billion doubles using PHP', PHP_EOL;
+var_dump(min($arr));
+echo 'Usage time:', (microtime(true) - $start) * 1000, 'ms', PHP_EOL;
+
+//Test Max
+$start = microtime(true);
+echo 'Calculate max of 10 billion doubles using C', PHP_EOL;
+var_dump($cUdf->ArrayMax($cArr, 100000000));
+echo 'Usage time:', (microtime(true) - $start) * 1000, 'ms', PHP_EOL;
+
+$start = microtime(true);
+echo 'Calculate max of 10 billion doubles using PHP', PHP_EOL;
+var_dump(max($arr));
 echo 'Usage time:', (microtime(true) - $start) * 1000, 'ms', PHP_EOL;
--- libs/udf/udf.c
+++ libs/udf/udf.c
@@ -11,3 +11,23 @@ double ArraySum(double numbers[], int size) {
 double ArrayAvg(double numbers[], int size) {
     return ArraySum(numbers, size) / size;
 }
+
+double ArrayMin(double numbers[], int size) {
+    double min = 0;
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] < min) {
+            min = numbers[i];
+        }
+    }
+    return min;
+}
+
+double ArrayMax(double numbers[], int size) {
+    double max = 0;
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
